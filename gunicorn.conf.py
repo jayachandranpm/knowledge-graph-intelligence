7624cf1fa44339e6b7fa0f9933b76caf964254f2
jayachandran.pm <jayachandran.pm@zohocorp.com>
2026-09-22 14:31:26 +0530

    Fit AppSail worker count to development memory

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,7 +3,7 @@ import os
 
 appsail_port = os.getenv("X_ZOHO_CATALYST_LISTEN_PORT", "5000")
 bind = os.getenv("GUNICORN_BIND", f"0.0.0.0:{appsail_port}")
-workers = int(os.getenv("GUNICORN_WORKERS", "2"))
+workers = int(os.getenv("GUNICORN_WORKERS", "1"))
 worker_class = "sync"
 timeout = 120
 keepalive = 5
